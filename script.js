f87065f31fefae2d35e1675b776b603bb9117d2e
Carlos <carlos96831@gmail.com>
2021-08-21 20:50:59 -0700

    added some script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var comment = document.getElementById("password");
+generateBtn.addEventListener('click', writePassword)
 
 // Write password to the #password input
 function writePassword() {
@@ -13,3 +14,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  var numbers = confirm("choose a length of at least 8 characters and no more than 128 characters")
+  
+  var lowerCases = confirm("")
+  
+  var upperCases = confirm("")
+
+}
